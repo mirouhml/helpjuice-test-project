f9299e04d984b5281a1dabdf59f62fe6d0c41fb5
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-10-28 04:07:01 +0100

    Add components to main page.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TopBar from './components/top-bar/top-bar.component';
+import Editor from './components/editor/editor.component';
+
+import './App.css';
+const App = () => {
+  return (
+    <div className='App'>
+      <TopBar />
+      <Editor />
+    </div>
+  );
+};
+
+export default App;
